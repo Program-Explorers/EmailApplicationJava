77e2d25a572b3b3af6ae0d557d177851231bac46
FlashCoder101 <anishwestwindsor@gmail.com>
2020-06-27 18:32:52 -0400

    Added password random generator

--- src/Email.java
+++ src/Email.java
@@ -1,11 +1,11 @@
-
 //import statements
 import java.util.*;
 
 public class Email {
     public static void main(String[] args) {
         System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-        String email = "", firstName, lastName, company;
+        String email = "", firstName, lastName, company, 
+        chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
         Scanner keyboard = new Scanner(System.in);
 
         System.out.print("What is your first name? ");
@@ -21,10 +21,20 @@ public class Email {
         company = keyboard.nextLine();
         email = email + company.toLowerCase();
         email = email + ".com";
+        
+        Random rand = new Random();
+        int password = rand.nextInt(3)+5;
+        // for(int i = 0; i < password; i++){
+        //     String 
+        // }
+        System.out.print(password);
 
+
+        
         System.out.print("\n");
         System.out.print(email);
         System.out.println("\n");
 
+      
     }
 }
