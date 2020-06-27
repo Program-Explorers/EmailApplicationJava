5255653f8f1a64f3aac95e648fae88d843d42984
Varun <varunk3249@gmail.com>
2020-06-27 19:04:32 -0400

    made password generator

--- src/Email.java
+++ src/Email.java
@@ -24,16 +24,25 @@ public class Email {
         
         Random rand = new Random();
         int password = rand.nextInt(3)+5;
-        // for(int i = 0; i < password; i++){
-        //     String 
-        // }
-        System.out.print(password);
-
-
+        int length = chars.length();
+        String userPassword = "";
         
-        System.out.print("\n");
+        System.out.print("\n\n\n\n\n\n\n\n\n\n\n\n\n");
+        System.out.print("Your company email is ");
         System.out.print(email);
         System.out.println("\n");
+
+        for(int i = 0; i < password; i++){
+            int numIndex = rand.nextInt(length);
+            char ch1 = chars.charAt(numIndex);
+            userPassword = userPassword + ch1;
+        }
+        System.out.print("Your password is ");
+        System.out.print(userPassword);
+        System.out.print("\n\n");
+       
+
+       
 
       
     }
