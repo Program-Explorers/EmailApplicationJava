4b033d44d855c9e20daed9c1b94215b6846d3a7a
Varun <varunk3249@gmail.com>
2020-06-27 18:13:00 -0400

    Asked user for input and made email with it

--- Email.java
+++ Email.java
@@ -1,10 +1,30 @@
+
 //import statements
 import java.util.*;
 
 public class Email {
-    public static void main(String [] args){
-        String email, firstName, lastName, department;
+    public static void main(String[] args) {
+        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+        String email = "", firstName, lastName, company;
         Scanner keyboard = new Scanner(System.in);
-        System.out.println("hello");
+
+        System.out.print("What is your first name? ");
+        firstName = keyboard.nextLine();
+        email = email + firstName.toLowerCase();
+
+        System.out.print("What is your last name? ");
+        lastName = keyboard.nextLine();
+        email = email + lastName.toLowerCase();
+        email = email + '@';
+
+        System.out.print("What is the name of your company? ");
+        company = keyboard.nextLine();
+        email = email + company.toLowerCase();
+        email = email + ".com";
+
+        System.out.print("\n");
+        System.out.print(email);
+        System.out.println("\n");
+
     }
 }
