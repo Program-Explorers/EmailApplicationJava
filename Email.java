dd5c405adb4e638a9e5b90ecbb15683be6899bae
FlashCoder101 <anishwestwindsor@gmail.com>
2020-06-27 17:58:02 -0400

    Made Email program file and added variables

---
+++ Email.java
@@ -0,0 +1,10 @@
+//import statements
+import java.util.*;
+
+public class Email {
+    public static void main(String [] args){
+        String email, firstName, lastName, department;
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("hello");
+    }
+}
